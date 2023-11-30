10ca8c084d1ecfa3f428c06b8a1d1a123ab0c123
SarthakYadav2911 <sarthakyadav2911@gmail.com>
2023-11-30 23:48:09 +0530

    first commit

---
+++ leevelone.java
@@ -0,0 +1,7 @@
+public class leevelone {
+    public static void main (String[] args ){
+        System.out.println("Hey there,");
+        System.out.println("i am data");
+    }
+    
+}
---
+++ levelonethird.java
@@ -0,0 +1,19 @@
+public class levelonethird {
+    public static void main(String[]args){
+        int j;
+      for(int i=1;i<=5;i++){
+        for(j=5;j>=i;j--)
+        {
+            System.out.print("x");
+           
+        }
+         if(j==4)
+            System.out.print(" "+"x"+" ");
+      System.out.println();  
+    }
+
+    }
+    
+}
+    
+
