b40edcc4458d86321660208b150366507bc7f25f
SarthakYadav2911 <135861850+SarthakYadav2911@users.noreply.github.com>
2023-12-09 19:41:34 +0530

    Add files via upload

---
+++ Levelonefifth.java
@@ -0,0 +1,10 @@
+public class Levelonefifth {
+    public static void main(String [] args)
+    {
+        int i=45;
+        int j=32;
+        System.out.println("difference"+ "= "+ (i-j));
+        System.out.println("product"+"= "+ (i*j));
+    }
+    
+}
---
+++ Leveonefourth.java
@@ -0,0 +1,11 @@
+public class Leveonefourth {
+    public static void main(String [] args)
+    {
+        int i=2;
+        int j=4;
+        int k=5;
+        int result=i+j+k;
+        System.out.println(result);
+    }
+    
+}
